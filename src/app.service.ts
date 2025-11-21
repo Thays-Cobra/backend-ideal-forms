893e6ebe4610a4ed7ed4485192a575d2200a2c44
Thays Cobra Perez <thays.perez.13@gmail.com>
2025-11-21 15:59:21 -0300

    doc: add comments and new scripts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
+//@Injectable() = decorator que marca a classe AppService como "injetável", que significa que pode ser usada pelo sistema de injeção de dependências do Nestjs
 @Injectable()
 export class AppService {
   getHello(): string {
